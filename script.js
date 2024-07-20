134869e479144e2995b6505392d7569f1af3fd2f
rajeshmondal54 <rm4223536@gmail.com>
2024-07-21 03:06:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+function moveToNext(current, nextFieldID) {
+    if (current.value.length === 1) {
+        document.getElementById(nextFieldID).focus();
+    }
+}
+
+function moveToPrev(current, prevFieldID, event) {
+    if (event.key === 'Backspace' && current.value.length === 0) {
+        document.getElementById(prevFieldID).focus();
+    }
+}
